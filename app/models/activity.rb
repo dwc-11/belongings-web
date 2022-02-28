76912effd16e6cbda313a3afb8896ffa40c4adcd
EC2 Default User <ec2-user@ip-172-31-0-183.ec2.internal>
2022-02-28 02:04:43 +0000

    form

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,7 @@
 class Activity < ApplicationRecord
     has_many :relations
     has_many :items , through: :relations
+    def self.search(keyword)
+    where(["name like?", "%#{keyword}%"])
+    end
 end
